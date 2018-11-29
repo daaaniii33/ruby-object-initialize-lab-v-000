d032fa67f59f7390dcd0ce28d46151e45fd2d8b9
daaaniii33 <daaaniii33@gmail.com>
2018-11-29 20:02:23 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class dog
+  def initialize(name, breed="Mutt")
+    @breed = breed
+    @name = name
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+end
+
+def breed=(breed)
+  @breed = breed
+end
